a223f3bd983fdd072ffac1e4f3f28c42e0ab1e8f
SaduAlbert <sadussky@163.com>
2017-05-15 15:39:06 +0800

    react natvie录音模块修改

--- ProRedux/src/pages/TestAudioToolkit.js
+++ ProRedux/src/pages/TestAudioToolkit.js
@@ -19,7 +19,10 @@ import {
     NativeModules,
     DeviceEventEmitter,
     NativeAppEventEmitter,
-    Platform
+    Platform,
+    ListView,
+    TouchableHighlight,
+    RefreshControl
 } from 'react-native';
 var RCTAudioRecorder = NativeModules.AudioRecorder;
 import Button from 'react-native-button';
@@ -40,30 +43,28 @@ let filename = 'test.mp4';
 class TestAudioToolkit extends React.Component {
     constructor() {
         super();
-
+        this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.voiceRecords = [];
+        this.voiceRecordCur = null;
         this.state = {
             playPauseButton: 'Preparing...',
             recordButton: 'Preparing...',
-
             stopButtonDisabled: true,
             playButtonDisabled: true,
             recordButtonDisabled: true,
-
             loopButtonStatus: false,
             progress: 0,
-
-            error: null
+            error: null,
+            dataSource: this.ds.cloneWithRows(this.voiceRecords),
+            refresh:false
         };
-
     }
 
     componentWillMount() {
         this.player = null;
         this.recorder = null;
         this.lastSeek = 0;
-        this.latelyLocalFileName = this._generateFileName();
-        this._reloadPlayer();
-        this._reloadRecorder();
+        this._prepareRecorder();
         this._progressInterval = setInterval(() => {
             if (this.player && this._shouldUpdateProgressBar()) {// && !this._dragging) {
                 this.setState({progress: Math.max(0, this.player.currentTime) / this.player.duration});
@@ -72,9 +73,16 @@ class TestAudioToolkit extends React.Component {
     }
 
     componentWillUnmount() {
-        //console.log('unmount');
-        // TODO
         clearInterval(this._progressInterval);
+    }
+
+
+    _prepareRecorder() {
+        if (this.recordFileName)
+            this.playerFileName = this.recordFileName;
+        this.recordFileName = StringUtils.uuid() + '.mp4';
+        // this._reloadPlayer(this.playerFileName);
+        this._reloadRecorder(this.recordFileName);
     }
 
     _shouldUpdateProgressBar() {
@@ -86,7 +94,6 @@ class TestAudioToolkit extends React.Component {
         this.setState({
             playPauseButton: this.player && this.player.isPlaying ? 'Pause' : 'Play',
             recordButton: this.recorder && this.recorder.isRecording ? 'Stop' : 'Record',
-
             stopButtonDisabled: !this.player || !this.player.canStop,
             playButtonDisabled: !this.player || !this.player.canPlay || this.recorder.isRecording,
             recordButtonDisabled: !this.recorder || (this.player && !this.player.isStopped),
@@ -114,22 +121,18 @@ class TestAudioToolkit extends React.Component {
         if (!this.player) {
             return;
         }
-
         this.lastSeek = Date.now();
-
         let position = percentage * this.player.duration;
-
         this.player.seek(position, () => {
             this._updateState();
         });
     }
 
-    _reloadPlayer() {
+    _reloadPlayer(fileName) {
         if (this.player) {
             this.player.destroy();
         }
-
-        this.player = new Player(this.latelyLocalFileName, {
+        this.player = new Player(fileName, {
             autoDestroy: false
         }).prepare((err) => {
             if (err) {
@@ -138,12 +141,9 @@ class TestAudioToolkit extends React.Component {
             } else {
                 this.player.looping = this.state.loopButtonStatus;
             }
-
             this._updateState();
         });
-
         this._updateState();
-
         this.player.on('ended', () => {
             this._updateState();
         });
@@ -152,11 +152,11 @@ class TestAudioToolkit extends React.Component {
         });
     }
 
-    _reloadRecorder() {
+    _reloadRecorder(fileName) {
         if (this.recorder) {
             this.recorder.destroy();
         }
-        this.recorder = new Recorder(this.latelyLocalFileName, {
+        this.recorder = new Recorder(fileName, {
             bitrate: 256000,
             channels: 2,
             sampleRate: 44100,
@@ -180,10 +180,8 @@ class TestAudioToolkit extends React.Component {
             }
             if (stopped) {
                 if (err) {
-                    this.latelyStoreFileURIString = '';
-                    this.latelyLocalFileName = this._generateFileName();
-                    this._reloadPlayer();
-                    this._reloadRecorder();
+                    this.recordFileStoreURI = '';
+                    this._prepareRecorder();
                 } else {
                     this._getFilePathAndUploadFileToServer();
                 }
@@ -208,12 +206,12 @@ class TestAudioToolkit extends React.Component {
 
     _getFilePathAndUploadFileToServer() {
         RCTAudioRecorder.getRecordFile(
-            this.latelyLocalFileName,
+            this.recordFileName,
             {},
             (uriString) => {
                 console.log(LOG_TAG, `_getFilePathAndUploadFileToServer %filePath%=${uriString}`);
-                this.latelyStoreFileURIString = uriString;
-                this._uploadVoiceFile(this.latelyLocalFileName, this.latelyStoreFileURIString);
+                this.recordFileStoreURI = uriString;
+                this._uploadVoiceFile(this.recordFileName, this.recordFileStoreURI);
             }
         );
     }
@@ -244,16 +242,36 @@ class TestAudioToolkit extends React.Component {
 
         SATFetch.post(url, body, header, true)
             .then((response) => {
-                this.latelyLocalFileName = this._generateFileName();
-                this._reloadPlayer();
-                this._reloadRecorder();
+                console.log(LOG_TAG, 'SATFetch.post resolve');
+                this.voiceRecords.push({
+                    name: name,
+                    selected: false,
+                    uri: fileURIString
+                });
+                console.log(LOG_TAG, `voiceRecords=${JSON.stringify(this.voiceRecords)}`);
+                this.setState({
+                    dataSource: this.ds.cloneWithRows(this.voiceRecords),
+                })
+
+                this._prepareRecorder();
             }).catch((err) => {
+            this._prepareRecorder();
         });
+
+    }
+
+
+    _onEndReached() {
+
+    }
+
+    _onRefreshData() {
+
     }
 
     render() {
         return (
-            <View>
+            <View style={{flex: 1, flexDirection: 'column'}}>
                 {this.renderContent()}
             </View>
         )
@@ -262,25 +280,25 @@ class TestAudioToolkit extends React.Component {
 
     renderContent() {
         return (
-            <View>
+            <View style={{flex: 1, flexDirection: 'column'}}>
                 <View>
                     <Text style={styles.text_title}>
                         Playback
                     </Text>
                 </View>
-                <View style={styles.btn_layout}>
-                    <TouchableOpacity
-                        disabled={this.state.playButtonDisabled}
-                        onPress={() => this._playPause()}>
-                        <Text style={styles.text_btn}>{this.state.playPauseButton}</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        disabled={this.state.playButtonDisabled}
-                        style={styles.button}
-                        onPress={() => this._stop()}>
-                        <Text style={styles.text_btn}>Stop</Text>
-                    </TouchableOpacity>
-                </View>
+                {/*  <View style={styles.btn_layout}>
+                 <TouchableOpacity
+                 disabled={this.state.playButtonDisabled}
+                 onPress={() => this._playPause()}>
+                 <Text style={styles.text_btn}>{this.state.playPauseButton}</Text>
+                 </TouchableOpacity>
+                 <TouchableOpacity
+                 disabled={this.state.playButtonDisabled}
+                 style={styles.button}
+                 onPress={() => this._stop()}>
+                 <Text style={styles.text_btn}>Stop</Text>
+                 </TouchableOpacity>
+                 </View>*/}
 
                 <View style={styles.settingsContainer}>
                     <Switch
@@ -307,16 +325,114 @@ class TestAudioToolkit extends React.Component {
                     </TouchableOpacity>
 
                 </View>
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity
-                        style={[styles.button, {marginTop: 20}]}
-                        onPress={() => this._getFilePathAndUploadFileToServer()}>
-                        <Text style={styles.text_btn}>UploadFile</Text>
-                    </TouchableOpacity>
-                </View>
+
+
+                {this.renderListView()}
+                {/*<View style={styles.buttonContainer}>
+                 <TouchableOpacity
+                 style={[styles.button, {marginTop: 20}]}
+                 onPress={() => this._getFilePathAndUploadFileToServer()}>
+                 <Text style={styles.text_btn}>UploadFile</Text>
+                 </TouchableOpacity>
+                 </View>*/}
                 <Text style={styles.errorMessage}>{this.state.error}</Text>
             </View>
         )
+    }
+
+    renderListView() {
+        return (
+            <ListView
+                style={{flex: 1, flexDirection: 'column'}}
+                enableEmptySections={true}
+                initialListSize={10}
+                onChangeVisibleRows={(visibleRows, changedRows) => {
+                }}
+                onEndReached={(...args) => this._onEndReached(...args)}
+                onEndReachedThreshold={20}
+                removeClippedSubviews={true}
+                //renderFooter={this._renderFooter}
+                //renderHeader={this._renderHeader}
+                //renderScrollComponent={this._renderScrollComponent}
+                //renderSectionHeader={this._renderSectionHeader}
+                renderSeparator={this._renderSeparator}
+                renderRow={(...args) => {
+                    return this._renderRow(...args)
+                }}
+                dataSource={this.state.dataSource}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={this.state.refresh}
+                        onRefresh={() => this._onRefreshData()}
+                    />
+                }
+            />
+        );
+    }
+
+
+    _renderHeader() {
+        //TODO
+    }
+
+    _renderFooter() {
+        //TODO
+    }
+
+    _renderScrollComponent(props) {
+        //TODO
+    }
+
+    _renderSectionHeader() {
+        //TODO
+    }
+
+    _renderRow(rowData, sectionID, rowID, highlightRow = (sectionID, rowID) => {
+        return null;
+    }) {
+        return (
+            <TouchableHighlight onPress={() => {
+                this._pressRow(rowData);
+                highlightRow(sectionID, rowID);
+            }}>
+                <View>
+                    <View style={styles.row}>
+                        <Text style={styles.text}>
+                            {rowData.name}
+                        </Text>
+                    </View>
+                </View>
+            </TouchableHighlight>
+        );
+    }
+
+    _pressRow(rowData) {
+        if (this.voiceRecordCur && this.voiceRecordCur.name == rowData.name) {
+            this._playPause();
+        } else if (this.voiceRecordCur) {
+            this._stop();
+            this.voiceRecordCur.selected = false;
+            rowData.selected = true;
+            this.voiceRecordCur = rowData;
+            this._reloadPlayer(rowData.name);
+            this._playPause();
+        } else {
+            rowData.selected = true;
+            this.voiceRecordCur = rowData;
+            this._reloadPlayer(rowData.name);
+            this._playPause();
+        }
+    }
+
+    _renderSeparator(sectionID, rowID, adjacentRowHighlighted) {
+        return (
+            <View
+                key={`${sectionID}-${rowID}`}
+                style={{
+                    height: adjacentRowHighlighted ? 4 : 1,
+                    backgroundColor: adjacentRowHighlighted ? '#3B5998' : '#CCCCCC',
+                }}/>
+        );
     }
 }
 
@@ -370,7 +486,17 @@ var styles = StyleSheet.create({
         borderWidth: 0.5,
         borderColor: '#d6d7da',
     },
-
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        padding: 10,
+        backgroundColor: '#F6F6F6',
+    },
+    thumb: {
+        width: 64,
+        height: 64,
+    },
+    text: {},
 });
 
 export default TestAudioToolkit;
